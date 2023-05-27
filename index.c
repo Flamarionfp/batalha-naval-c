1fbe9a59503c4e70da41f694f6701379dc8b7d2a
flamarionfp <flamarionpinto@sou.faccat.br>
2023-05-26 21:57:44 -0300

    feat: finish v 1.0

--- index.c
+++ index.c
@@ -1,15 +1,12 @@
-// Trabalho Flamarion Fagundes e Allan Rizi
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 
-#define BOARD_SIZE 9
+#define BOARD_SIZE 20
 #define SHIP_LENGTH 5
-#define SHIP_LIMIT 1
-#define SHOTS_LIMIT 2
-#define SHIPS_DESTROYED_TO_WIN 1
+#define SHIP_LIMIT 3
+#define SHOTS_LIMIT 30
 #define EMPTY_IDENTIFIER '~'
 #define SHIP_IDENTIFIER '#'
 #define SHOT_IDENTIFIER '*'
@@ -19,14 +16,14 @@ const char* LETTERS = "ABCDEFGHIJKLMNOPQRST";
 char players[2][100];
 int currentPlayerIndex = 0;
 int wonRoundsByPlayer[2];
-int shipsDestroyedByPlayer[2];
+int shipsDestroyedByPlayer[2] = {0, 0}; 
 int positionsHitByPlayer[2];
 
 int shipBoard[BOARD_SIZE][BOARD_SIZE];
 int shotBoard[BOARD_SIZE][BOARD_SIZE];
-int shipsDestroyed = 0;
+int hasDestroyedAll = 0;
 
-// isso aqui vai ser definido a nível de compilação para rodar o comando de limpar a tela de acordo com a plataforma
+// isso aqui vai ser definido a nível de compilação para rodar o comando de limpar a tela de acordo com o ambiente de execução
 void clearScreen() {
   #ifdef _WIN32
     system("cls");
@@ -61,34 +58,34 @@ void initBoards() {
   }
 }
 
-// se der tempo, refatorar essa função para simplificar as condições (quebrar em funções menores)
 void drawBoard(int isShipBoard) {
-  printf("  ");
+  printf("   ");
   for (int i = 0; i < BOARD_SIZE; i++) {
-    printf("%d ", i + 1);
+    printf("%3d", i + 1); // adicionei o 3d pra ficar mais espaçado, como é 20x20 o tabuleiro
   }
   printf("\n");
 
   for (int i = 0; i < BOARD_SIZE; i++) {
-    printf("%c ", LETTERS[i]);
+    printf("%c  ", LETTERS[i]);
     for (int j = 0; j < BOARD_SIZE; j++) {
       if (isShipBoard) {
         if (shipBoard[i][j] == 0)
-          printf("%c ", EMPTY_IDENTIFIER);
+          printf("%3c", EMPTY_IDENTIFIER);
         else
-          printf("%c ", SHIP_IDENTIFIER);
+          printf("%3c", SHIP_IDENTIFIER);
       } else {
         if (shotBoard[i][j] == 0)
-          printf("%c ", EMPTY_IDENTIFIER);
+          printf("%3c", EMPTY_IDENTIFIER);
         else if (shotBoard[i][j] == 1)
-          printf("%c ", SHOT_IDENTIFIER);
+          printf("%3c", SHOT_IDENTIFIER);
         else if (shotBoard[i][j] == 2)
-          printf("%c ", SHIP_IDENTIFIER);
+          printf("%3c", SHIP_IDENTIFIER);
       }
     }
     printf("\n");
   }
 }
+
 
 void buildShip(int startsAtColumn, int startsAtRow, char orientation) {  
   for (int i = startsAtColumn; i < SHIP_LENGTH + startsAtColumn; i++) { 
@@ -128,17 +125,37 @@ int verifyPlayAgain(char playAgain[]) {
   return strcasecmp(playAgain, "sair") != 0;
 }
 
-int verifyWin() {
-  return shipsDestroyed == SHIPS_DESTROYED_TO_WIN;
+int verifyLose() {
+  return shipsDestroyedByPlayer[currentPlayerIndex] < SHIP_LIMIT;
 }
 
 void onWin() {
   wonRoundsByPlayer[currentPlayerIndex]++;
+  shipsDestroyedByPlayer[currentPlayerIndex] += SHIP_LIMIT;
+
   printf("Jogador %s venceu!\n", players[currentPlayerIndex]);
 }
 
 void onLose() {
   printf("Jogador %s perdeu!\n", players[currentPlayerIndex]);
+}
+
+int checkDestroyedAllShips() {
+  int shipsNotDestroyed = 0;
+
+  for (int i = 0; i < BOARD_SIZE; i++) {
+    for (int j = 0; j < BOARD_SIZE; j++) {
+      if (shipBoard[i][j] == 1) {
+        shipsNotDestroyed = 1;
+        return 0;
+      }
+    }
+    if (shipsNotDestroyed) {
+      return 0;
+    }
+  }
+
+  return !shipsNotDestroyed;
 }
 
 void onHitShot(int row, int column) {
@@ -159,13 +176,19 @@ void makePlay(int row, int column) {
   } else {
     onMissedShot(row, column);
   }
+  
+  hasDestroyedAll = checkDestroyedAllShips();
+
+  if (hasDestroyedAll) {
+    onWin();
+  }
 }
 
 void askToPlay() {
   int shotsRemaining = SHOTS_LIMIT;
   int row, column;
 
-  while (shotsRemaining > 0) {
+  while (shotsRemaining > 0 && hasDestroyedAll != 1) {
     printf("%s, informe a linha do tiro: ", players[currentPlayerIndex]);
     scanf("%d", &row);
 
@@ -191,34 +214,34 @@ void startGame() {
 void showGameReport() {
   printf("\n");
   for (int i = 0; i < 2; i++) {
+    printf("\n");
     printf("Jogador %s acertou %d tiro(s)\n", players[i], positionsHitByPlayer[i]);
+    printf("Jogador %s destruiu %d navio(s)\n", players[i], shipsDestroyedByPlayer[i]);
     printf("Jogador %s venceu %d rodada(s)\n", players[i], wonRoundsByPlayer[i]);
   }
 }
 
 int main() {
   char playAgain[100];
-  int win;
+  int lose;
 
   startGame();
 
   do {
     initBoards();
     askToBuildShips();
-    sleep(2); // Adicionei esse sleep para o jogador poder ver o último barco posicionado antes de limpar a tela
+    sleep(2); // Adicionei esse sleep para o jogador poder ver o último barco posicionado por alguns segundos antes de limpar a tela
     askToClearScreen();
     changePlayer(); // troca para o jogador que vai atirar
     drawBoard(0);  
     askToPlay();
 
-    win = verifyWin();
+    lose = verifyLose();
 
-    if (win) {
-      onWin();
-    } else {
+    if (lose) {
       onLose();
     }
-
+    
     printf("Jogar novamente ou sair? ");
     scanf("%s", playAgain);
   } while (verifyPlayAgain(playAgain));
